ef9b9a4f3742f17e57e44d0d3bcde6c1070b038e
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-06-12 21:34:29 +0200

    6.2 TDD

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(20,7);
+        int resul2 = calculator.subtract(20,7);
+
+        if (result == 27) {
+            System.out.println("Correct");
+        }else {
+            System.out.println("Incorrect");
+        }
+        if (resul2 == 13) {
+            System.out.println("Correct");
+        }else {
+            System.out.println("Incorrect");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a = 20;
+    int b = 7;
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
