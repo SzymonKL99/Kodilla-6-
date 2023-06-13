8395efe02910f81db1ec4fc72aa0a899aae924b8
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-06-13 20:26:07 +0200

    6.3 JUnit first test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.collection;
+import java.util.List;
+import java.util.ArrayList;
+public class OddNumbersExterminator {
+    public static List<Integer> exterminate(List<Integer> numbers) {
+        ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+        if (numbers.isEmpty()) {
+            return numbers;
+        } else {
+            for (Integer number : numbers) {
+                if (number % 2 == 0) {
+                    oddNumbers.add(number);
+                }
+            }
+            return oddNumbers;
+        }
+    }
+
+}
